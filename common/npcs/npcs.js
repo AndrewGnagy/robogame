2bf45c1c97413a762c393d3a8eb2b794c06cba94
AndrewGnagy <agnagy89@gmail.com>
2014-02-16 17:27:20 -0500

    move all to common library

---
+++ common/npcs/npcs.js
@@ -0,0 +1,49 @@
+NPCs = {
+    "guy": {
+        name: "guy",
+        image: "img/guy32.png",
+        width: 32,
+        height: 38,
+		action: function (){
+			if(character.hasItem("wrench") || character.saved.story.wrench){
+				dialog.show(["Thanks for the wrench!"]);
+				character.saved.story.wrench = true;
+				character.removeItem("wrench");
+			}
+			else
+				dialog.show(["Bring me a wrench"]);
+		}
+    },
+    "bob": {
+        name: "Bob",
+        image: "img/guy32.png",
+        width: 32,
+        height: 38,
+		action: function (){
+			dialog.show(["Prepare to be worsted!"]);
+			//TODO Initiate battle here
+		}
+    },
+    "captainHammer": {
+        name: "captainHammer",
+        image: "img/captainHammer32.png",
+        width: 32,
+        height: 38,
+		action: function (){
+			dialog.show(["I'm Captain Hammer,", "Corporate Tool"], function(){
+				var player2 = new Character("captainHammer");
+
+				robo.battleObject = new battleScene(character, player2);
+
+				clearInterval(robo.currentInterval);
+
+				var robot2 = new robotObject();
+				player2.addRobot(robot2);
+				robot2.loadRobot("527546fa41f3ec7af56855ef");
+
+				stage.clear();
+				finishBattle();
+			});
+		}
+    }
+}
