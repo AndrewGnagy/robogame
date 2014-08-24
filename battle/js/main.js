0232a392362d1aee08db9518f38ece7d69161fe5
Arjun-Kiran <ajkiran1@gmail.com>
2014-08-24 12:11:30 -0400

    Battle Testing
    make battle testing site emulate the look and feel of actual main app

--- battle/js/main.js
+++ battle/js/main.js
@@ -2,56 +2,68 @@
 WIDTH = 768;
 HEIGHT = 512;
 
+var character = new Character('hero');
+
+function startgame()
+{
+	player1 = new Character();
+	player1.name = "Arjun";
 
 
-player1 = new Character();
-player1.name = "Arjun";
+	player2 = new Character();
+	player2.name = "Andrew";
 
 
-player2 = new Character();
-player2.name = "Andrew";
+	robot1 = buildRobot(RobotJson,"HellRaiser");
+	robot2 = buildRobot(RobotJson,"RC-0022");
+	robot3 = buildRobot(RobotJson,"Rampage");
+	robot4 = buildRobot(RobotJson,"HellRaiser");
+	robot5 = buildRobot(RobotJson,"HellRaiser");
+	robot6 = buildRobot(RobotJson,"HellRaiser");
 
+	robot1.learnAttack("Triple Slash Attack");
+	robot1.learnAttack("Drill");
 
-robot1 = buildRobot(RobotJson,"HellRaiser");
-robot2 = buildRobot(RobotJson,"RC-0022");
-robot3 = buildRobot(RobotJson,"Rampage");
-robot4 = buildRobot(RobotJson,"HellRaiser");
-robot5 = buildRobot(RobotJson,"HellRaiser");
-robot6 = buildRobot(RobotJson,"HellRaiser");
+	robot2.learnAttack("Entropy Increase");
+	robot2.learnAttack("Supreme Blast");
 
-robot1.learnAttack("Triple Slash Attack");
-robot1.learnAttack("Drill");
+	robot3.learnAttack("Positron Beam");
 
-robot2.learnAttack("Entropy Increase");
-robot2.learnAttack("Supreme Blast");
+	robot4.learnAttack("Triple Slash Attack");
+	robot4.learnAttack("Drill");
 
-robot3.learnAttack("Positron Beam");
+	robot5.learnAttack("Entropy Increase");
+	robot5.learnAttack("Supreme Blast");
 
-robot4.learnAttack("Triple Slash Attack");
-robot4.learnAttack("Drill");
+	robot6.learnAttack("Positron Beam");
 
-robot5.learnAttack("Entropy Increase");
-robot5.learnAttack("Supreme Blast");
+	player1.addRobot(robot1);
+	player1.addRobot(robot3);
+	player1.addRobot(robot2);
 
-robot6.learnAttack("Positron Beam");
+	player2.addRobot(robot5);
+	player2.addRobot(robot4);
+	player2.addRobot(robot6);
 
-player1.addRobot(robot1);
-player1.addRobot(robot3);
-player1.addRobot(robot2);
+	var stage = new Kinetic.Stage({
+			container: 'container',
+			width: WIDTH,
+			height: HEIGHT
+		});
 
-player2.addRobot(robot5);
-player2.addRobot(robot4);
-player2.addRobot(robot6);
+	battleObject = new battleScene(player1, player2);
+	battleObject.main(stage);
+	var timer = setInterval(function(){
+		if(!battleObject.loop()) clearInterval(timer);;
+	},150);
+}
 
-var stage = new Kinetic.Stage({
-		container: 'container',
-		width: WIDTH,
-		height: HEIGHT
-	});
+function loadUser()
+{
+	startgame();
+}
 
-battleObject = new battleScene(player1, player2);
-battleObject.main(stage);
-var timer = setInterval(function(){
-	if(!battleObject.loop()) clearInterval(timer);;
-},150);
-
+function saveUser()
+{
+	console.log("empty");
+}
